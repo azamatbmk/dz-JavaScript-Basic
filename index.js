5707231165a15e3c0a923399b90120de6cf7e08f
Azamat Kudziev <azamat.kudziev@gmail.com>
2024-07-10 07:57:35 +0300

    Решение задачи с растоянием

---
+++ index.js
@@ -0,0 +1,6 @@
+function distanceCalc(posLat, posLong, destLat, destLong) {
+    let distance = (destLat - posLat)**2 + (destLong - posLong)**2
+    return Math.sqrt(distance)
+}
+
+console.log(distanceCalc(13, 43, 13, 5))
