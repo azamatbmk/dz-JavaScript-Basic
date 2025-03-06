293797748a1cd1fcaf4317566dae9e1e378a5085
Azamat Kudziev <azamat.kudziev@gmail.com>
2025-03-06 18:19:58 +0300

    Функция высшего порядка

---
+++ 10-array-clean/index.js
@@ -0,0 +1,20 @@
+let newArr = [ 3, 6, 12, 45, 5, 21, 34, 56, 97, 4, 11, 9, 15, 1]
+
+function deleteOrNot(num) {
+    if(num > 10) {
+        return false
+    }
+    return true
+};
+
+function deleteItem(arr, fn) {
+    let emptyArr = [];
+    for(let el of arr) {
+        if(fn(el)) {
+            emptyArr.push(el)
+        }
+    }
+    return emptyArr;
+}
+
+console.log(deleteItem(newArr, deleteOrNot))
