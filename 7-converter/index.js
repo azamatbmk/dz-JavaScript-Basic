07b8c38e20b9fbc108d49488a7ad2026678e1400
Azamat Kudziev <azamat.kudziev@gmail.com>
2025-03-07 19:25:49 +0300

    Исправил конвертер

--- 7-converter/index.js
+++ 7-converter/index.js
@@ -1,15 +1,30 @@
 function converter (sum, currency, targetCurrency) {
-   switch (currency === 'RUB') {
-    case (targetCurrency === 'USD'):
-        sum = sum * 90;
-        break;
-    case (targetCurrency === 'EUR'):
-        sum = sum * 100;
-        break;
-    default:
-        sum = null        
+   switch (true) {
+        case (currency === 'RUB' && targetCurrency === 'USD'):
+            sum = sum / 90;
+            break;
+        case (currency === 'RUB' && targetCurrency === 'EUR'):
+            sum = sum * 100;
+            break;
+        case (currency === 'USD' && targetCurrency === 'RUB'):
+            sum = sum * 90;
+            break;
+        case (currency === 'USD' && targetCurrency === 'EUR'):
+            sum = sum * 0.92;
+            break;
+        case (currency === 'EUR' && targetCurrency === 'USD'):
+            sum = sum * 1.08;
+            break;
+        case (currency === 'EUR' && targetCurrency === 'RUB'):
+            sum = sum * 96;
+            break;
+        case (currency === targetCurrency):
+            sum = sum;
+            break;
+        default:
+            sum = null        
    }
    return sum;
 }
 
-console.log(converter(12654654, 'RUB', 'YEN'));
+console.log(converter(100, 'EUR', 'USD'));
