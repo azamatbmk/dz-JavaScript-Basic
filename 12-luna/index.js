b71b9a348916d76898e2f149ea40073f207b2e75
Azamat Kudziev <azamat.kudziev@gmail.com>
2025-03-08 17:41:31 +0300

    Алгоритм Луна
    Без поисков в интернете н есправился бы

---
+++ 12-luna/index.js
@@ -0,0 +1,25 @@
+let card = '4561-2612-1234-5464'
+let card2 = '4276-6000-5250-7189'
+
+function validateCard(cardNumber) {
+    let number = cardNumber.split('-').join('');
+    
+    let sum = 0;
+    let mustMultiplied = false;
+
+    for (let i = number.length - 1; i >= 0; i-- ) {
+        let digit = Number(number[i]);
+
+        if (mustMultiplied) {
+            digit *= 2;
+            if (digit > 9) {
+                digit = digit - 9;
+            }
+        }
+        sum += digit;
+        mustMultiplied = !mustMultiplied;
+    }
+    return sum % 10 === 0;
+}
+
+console.log(validateCard(card2))
